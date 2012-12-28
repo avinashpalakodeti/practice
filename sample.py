5bda91ee3c0637a0c986550a1dd5b9b50e44df58
fads <fads1@fads1-Lenovo-G570.(none)>
2012-12-28 14:07:47 +0530

    modified

--- sample.py
+++ sample.py
@@ -6,10 +6,16 @@ class Paginator(object):
 
 	
 	def total_pages(self):
-		if(self.items%self.items_per_page==0):
+		if self.items==0:
+			print "0 per page"		
+		elif self.items%self.items_per_page==0:
 			print self.items/self.items_per_page
 		else:
 			print (self.items/self.items_per_page)+1
+		if self.items<self.items_per_page and self.items>1:
+			print "1 per page"
+			
+			
 
 
 	def total_items(self):
@@ -27,7 +33,7 @@ class Paginator(object):
 		for ind,itm in enumerate(mylist):
 			print("x["+str(ind)+"]="+str(itm)) 
 
-page=Paginator(0,5)
+page=Paginator(100,2)
 page.total_pages()
 page.total_items()
 page.page_items(2,page.mylist)
